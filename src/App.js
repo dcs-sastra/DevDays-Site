64e85020e460e471c8617f877699e147da5561ce
ShyamSunder149 <SubstantialCitron0@protonmail.com>
2022-04-26 09:44:58 +0530

    css changes,footer implemetation,title change

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import './App.css';
+import { Helmet } from 'react-helmet';
 import Home from "./Pages/home"
 function App() {
   return (
     <>
+        <Helmet>
+      <title>Dev Days 22</title>
+    </Helmet>
     <Home/>
   </>
   );
--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,12 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from "../assets/logo.png"
-import {Row, Col} from "react-bootstrap"
+import { Row, Col, Container } from "react-bootstrap"
+import './css/footer.css'
 class Footer extends Component {
-    render(){
-        return(
-            <>
-                
-            </>
+    render() {
+        return (
+            <div className="footer">
+                <Container>
+                    <div className="row">
+                        <div className="col-lg-4 col-md-4 col-sm-12 text-center">
+                            <img src={logo} width="40%" height="auto" className="mt-5"></img>
+                        </div>
+                        <div className="col-lg-8 col-md-8 col-sm-12">
+                            <h3 className="colortext mt-5 text-center ">Quick Links</h3>
+                            <ul className="text-center">
+                                <a href="/#organizers">Organizers</a><br />
+                                <a href='/#sponsors'>Sponsors</a><br />
+                                <a href='/#badge'>Badge</a><br />
+                                <a href='/#faq'>FAQ</a>
+                            </ul>
+                        </div>
+
+                    </div>
+                </Container>
+            </div>
         )
     }
 }
