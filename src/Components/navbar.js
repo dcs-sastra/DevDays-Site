646eed9afac4ef17b794360a43f4b84f053d139b
Sarveshgith <130198294+Sarveshgith@users.noreply.github.com>
2025-02-04 19:06:04 +0530

    responsiveness fix

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,31 +1,39 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
-import './css/navbar.css'
-import logo from "../assets/Devdays Logo.svg"
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import './css/navbar.css';
+import logo from '../assets/Devdays Logo.svg';
 const navbar = () => {
   return (
-    <Navbar collapseOnSelect expand="md" bg="transparent" variant="light" className="navbar">
-      <Container>
-        <Navbar.Brand href="#home">
-          <img
-            src={logo}
-            alt="logo"
-            className=" d-none d-lg-block mt-3"
-            width="20%"
-            height="auto"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="text-center">
-            
-             <div className="p-2"> <Nav.Link href="/" className='hclass'>Home</Nav.Link></div>
-             <div className="p-2"> <Nav.Link href="/#schedule">Schedule</Nav.Link></div>
-             {/* <div className="p-2"> <Nav.Link href="/#badge">Badge</Nav.Link></div> */}
-             <div className="p-2"> <Nav.Link href="/#faq">FAQ</Nav.Link></div>
-             <div className="p-2"> <Nav.Link href="/#contact">Contact</Nav.Link></div>
-           
-          </Nav>
-        </Navbar.Collapse>
+    <Navbar collapseOnSelect expand='md' bg='transparent' variant='light' className='navbar'>
+      <Container className=''>
+        <div className='d-flex justify-content-between w-100'>
+          <Navbar.Brand href='#home'>
+            <img src={logo} alt='logo' className=' d-block d-md-block mt-3 px-2' id='logo' height='auto' />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='text-center'>
+              <div className='p-2'>
+                {' '}
+                <Nav.Link href='/' className='hclass'>
+                  Home
+                </Nav.Link>
+              </div>
+              <div className='p-2'>
+                {' '}
+                <Nav.Link href='/#schedule'>Schedule</Nav.Link>
+              </div>
+              {/* <div className="p-2"> <Nav.Link href="/#badge">Badge</Nav.Link></div> */}
+              <div className='p-2'>
+                {' '}
+                <Nav.Link href='/#faq'>FAQ</Nav.Link>
+              </div>
+              <div className='p-2'>
+                {' '}
+                <Nav.Link href='/#contact'>Contact</Nav.Link>
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );
--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,88 +1,80 @@
-import landingvector from "../assets/landingvector.svg";
-import { Container, Button } from "react-bootstrap";
-import Faq from "../Components/faq";
-import Navbar from "../Components/navbar";
-import Badge from "../Components/badge";
-import tendays from "../assets/days.svg";
-import fifteenworkshop from "../assets/workshop.svg";
-import logo from "../assets/Devdays Logo.svg";
+import landingvector from '../assets/landingvector.svg';
+import { Container, Button } from 'react-bootstrap';
+import Faq from '../Components/faq';
+import Navbar from '../Components/navbar';
+import Badge from '../Components/badge';
+import tendays from '../assets/days.svg';
+import fifteenworkshop from '../assets/workshop.svg';
+import logo from '../assets/Devdays Logo.svg';
 import Timeline from "../assets/TimeLine-Devdays'25.svg";
-import instagramicon from "../assets/instaicon.svg"
-import linkedinicon from "../assets/Linkedinicon.svg"
-import youtubeicon from "../assets/youtubeicon.svg"
-import twittericon from "../assets/Twitter.svg"
+import instagramicon from '../assets/instaicon.svg';
+import linkedinicon from '../assets/Linkedinicon.svg';
+import youtubeicon from '../assets/youtubeicon.svg';
+import twittericon from '../assets/Twitter.svg';
 
-import "./css/home.css";
+import './css/home.css';
 const Home = () => {
   return (
     <>
-      <div className="pt-2">
+      <div className='pt-2'>
         <Navbar />
       </div>
 
       {/* landing part  */}
-      <Container fluid className="my-5 home">
+      <Container fluid className='my-md-5 home'>
         <Container>
-          <div className="row">
-            <div className="col-lg-7 col-md-7 col-sm-12 mt-5 pt-5">
-              <h1 className="font fw-bold">
+          <div className='row'>
+            <div className='col-lg-7 col-md-7 col-sm-12 mt-md-5 pt-5'>
+              <h1 className='font fw-bold'>
                 The Developer and Innovation Festival of
-                <span className="colortext font"> SASTRA UNIVERSITY</span>
+                <span className='colortext font'> SASTRA UNIVERSITY</span>
               </h1>
-              <p className="pt-2 pb-4 col-lg-10 para h6">
-                We bring you a unique fest to band together the world of tech
-                and nascent student developers. Developer Days are a set of
-                tailored workshops, curated by tech aficionados to help you
-                scour the intricacies of the product development cycle from
-                scratch. Let's celebrate the joy of learning!
+              <p className='pt-2 pb-4 col-lg-10 para h6'>
+                We bring you a unique fest to band together the world of tech and nascent student developers. Developer
+                Days are a set of tailored workshops, curated by tech aficionados to help you scour the intricacies of
+                the product development cycle from scratch. Let's celebrate the joy of learning!
               </p>
-              <div className="pt-3">
-                <a href="https://forms.gle/Zqob1ZK4VMbtNPXP9" target="_blank" rel="noreferrer">
-                <Button className="rounded-pill btn px-4 py-3" size="md">
-                Interested? Click here
-                </Button>
+              <div className='pt-3'>
+                <a href='https://forms.gle/Zqob1ZK4VMbtNPXP9' target='_blank' rel='noreferrer'>
+                  <Button className='rounded-pill btn px-4 py-3' size='md'>
+                    Interested? Click here
+                  </Button>
                 </a>
               </div>
             </div>
-            <div className="col-lg-5 col-md-5 col-sm-12 mt-auto pt-5">
-              <img
-                src={landingvector}
-                alt=" landing-vector"
-                className="img-fluid"
-              />
+            <div className='col-lg-5 col-md-5 col-sm-12 mt-auto pt-5'>
+              <img src={landingvector} alt=' landing-vector' className='img-fluid' />
             </div>
           </div>
         </Container>
       </Container>
 
       {/* About part */}
-      <Container fluid className="about">
-        <Container className="pb-5">
-          <div className="row">
-            <div className="col-lg-7 col-md-7 col-sm-12 mt-auto pt-5 pl-2 ">
-              <h1 className="pe-3 font fw-bold">
-                About <span className="colortext">Dev Days</span>
+      <Container fluid className='about'>
+        <Container className='pb-5'>
+          <div className='row'>
+            <div className='col-lg-7 col-md-7 col-sm-12 mt-auto pt-5 pl-2 '>
+              <h1 className='pe-3 font fw-bold'>
+                About <span className='colortext'>DevDays</span>
               </h1>
-              <p className="pt-2 pb-2 font para h6">
-                Dev Days is a series of technical workshops organised by team
-                DCS, emphasising the Product-Development Cycle. It is all about
-                finding the perfect ways to inspire the community to learn and
-                explore new technologies. So, we present you with this festive
-                of workshops to transform your dreary weekends into enthralling
-                ones.
+              <p className='pt-2 pb-2 font para h6'>
+                DevDays is a series of technical workshops organised by team DCS, emphasising the Product-Development
+                Cycle. It is all about finding the perfect ways to inspire the community to learn and explore new
+                technologies. So, we present you with this festive of workshops to transform your dreary weekends into
+                enthralling ones.
               </p>
-              <div className="pt-3">
-                <a href="https://www.instagram.com/share/BAfp_D6yMg" target="_blank" rel="noreferrer">
-                <Button className="rounded-pill btn " size="md">
-                  Know more
-                </Button>
-</a>
+              <div className='pt-3'>
+                <a href='https://www.instagram.com/share/BAfp_D6yMg' target='_blank' rel='noreferrer'>
+                  <Button className='rounded-pill btn ' size='md'>
+                    Know more
+                  </Button>
+                </a>
               </div>
             </div>
 
-            <div className="col-lg-5 col-md-5 col-sm-12 my-5 ">
-              <div className="pt-5"></div>
-              <img src={logo} alt="DevDayslogo" className="img-fluid " />
+            <div className='col-lg-5 col-md-5 col-sm-12 my-5 '>
+              <div className='pt-5'></div>
+              <img src={logo} alt='DevDayslogo' className='img-fluid ' />
             </div>
           </div>
         </Container>
@@ -106,18 +98,15 @@ const Home = () => {
 
       {/* Timeline part */}
 
-      <Container fluid className="pt-5" id="schedule">
+      <Container fluid className='pt-5' id='schedule'>
         <Container>
-        <div className='row'>
-        <h1 className="text-center fw-bold pb-3 ">
-          DevDays <span className="colortext">Timeline</span>{" "}
-        </h1>
-        <img src={Timeline}
-        alt="Timeline"
-        className="img-fluid my-3"
-        />
-</div>
-</Container>
+          <div className='row'>
+            <h1 className='text-center fw-bold pb-3 '>
+              DevDays <span className='colortext'>Timeline</span>{' '}
+            </h1>
+            <img src={Timeline} alt='Timeline' className='img-fluid my-3' />
+          </div>
+        </Container>
       </Container>
 
       {/* Badge Part */}
@@ -127,58 +116,57 @@ const Home = () => {
       </Container> */}
 
       {/* Frequently asked part */}
-      <Container fluid className=" my-5" id="faq">
-        <Container >
+      <Container fluid className=' my-5' id='faq'>
+        <Container>
           <div>
-            <h1 className="pt-5 pb-5 fw-bold">
-              Frequently <span className="colortext">Asked Questions</span>
+            <h1 className='pt-5 pb-5 fw-bold'>
+              Frequently <span className='colortext'>Asked Questions</span>
             </h1>
           </div>
-          <div className="pb-5">
+          <div className='pb-5'>
             <Faq />
           </div>
         </Container>
       </Container>
 
       {/* Keep in touch part */}
-      <Container fluid id="contact" className="about">
-        <Container className="pt-5">
-        <div>
-          <h1 className="text-center fw-bold ">
-            Keep in touch with <span className="colortext">#team_dcs</span>
-          </h1>
-          <h1 className="text-center fw-bold "> for latest announcements</h1>
-          <p className="text-center para pt-2">
-            Let's get connected in social medias so that we can reach out to each other via a
-            simple notification. Stake out #team_dcs for the updates and news.
-          </p>
-        </div>
+      <Container fluid id='contact' className='about'>
+        <Container className='pt-5'>
+          <div>
+            <h1 className='text-center fw-bold '>
+              Keep in touch with <span className='colortext'>#team_dcs</span>
+            </h1>
+            <h1 className='text-center fw-bold '> for latest announcements</h1>
+            <p className='text-center para pt-2'>
+              Let's get connected in social medias so that we can reach out to each other via a simple notification.
+              Stake out #team_dcs for the updates and news.
+            </p>
+          </div>
 
-        <div className="d-flex flex-row flex-fill justify-content-center">
-
-          <div >
-            <a href="https://www.instagram.com/team_dcs_/" target="_blank">
-            <img src={instagramicon} style={{height:40, width:40}}  className="m-3 m-lg-5"/>
-            </a>
+          <div className='d-flex flex-row flex-fill justify-content-center'>
+            <div>
+              <a href='https://www.instagram.com/team_dcs_/' target='_blank'>
+                <img src={instagramicon} style={{ height: 40, width: 40 }} className='m-3 m-lg-5' />
+              </a>
+            </div>
+            <div>
+              <a href='https://twitter.com/dcs_sastra' target='_blank'>
+                <img src={twittericon} style={{ height: 40, width: 40 }} className='m-3 m-lg-5' />
+              </a>
+            </div>
+            <div>
+              <a href='https://www.linkedin.com/in/dcs-sastra/' target='_blank'>
+                <img src={linkedinicon} style={{ height: 40, width: 40 }} className='m-3 m-lg-5' />
+              </a>
+            </div>
+            <div>
+              <a href='https://www.youtube.com/c/DeveloperCommunitySASTRA' target='_blank'>
+                <img src={youtubeicon} style={{ height: 40, width: 40 }} className='m-3 m-lg-5' />
+              </a>
+            </div>
           </div>
-          <div >
-            <a href="https://twitter.com/dcs_sastra" target="_blank">
-            <img src={twittericon} style={{height:40, width:40}}  className="m-3 m-lg-5"/>
-            </a>
-          </div>
-          <div >
-            <a href="hhttps://www.linkedin.com/in/dcs-sastra/" target="_blank">
-            <img src={linkedinicon} style={{height:40, width:40}}  className="m-3 m-lg-5"/>
-            </a>
-          </div>
-          <div >
-            <a href="https://www.youtube.com/c/DeveloperCommunitySASTRA" target="_blank">
-            <img src={youtubeicon} style={{height:40, width:40}}  className="m-3 m-lg-5"/>
-            </a>
-          </div>
-        </div>
+        </Container>
       </Container>
-</Container>
     </>
   );
 };
